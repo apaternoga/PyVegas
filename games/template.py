3f8bd9453fb7d197adc030811598a108c23abadf
apaternoga <fatherleg@gmail.com>
2025-12-26 22:29:16 +0100

    Dodanie pliku template.py ze szkieletem nowej gry

--- games/template.py
+++ games/template.py
@@ -1,3 +1,15 @@
 import pygame
-from core.settings import SCREEN_WIDTH, SCREEN_HEIGHT, BLACK, WHITE
+from core.settings import *
 
+class GameTemplate:
+    def __init__(self,screen):
+        self.screen=screen
+        # inicjowanie zmiennych dla nowej gry
+
+    def handle_input(self,event):
+        pass
+        # obsluga klawiszy specficznych dla tej gry
+    
+    def draw(self):
+        self.screen.fill(BLACK) #przykladowe czyszczenie ekranu
+        #rysowanie elementow gry
